2e653c0707aa09ffe41d801d1ecdc260a0d6c111
wizardofhoms <wizardofhoms@proton.me>
2023-02-23 00:34:03 +0100

    Import/remove master private from backup only

--- src/private_import_command.sh
+++ src/private_import_command.sh
@@ -8,18 +8,23 @@ if ! is_hush_mounted ; then
     _failure "The hush device is not mounted. Mount it first and rerun the command."
 fi
 
+if ! is_luks_mapper_present "$BACKUP_MAPPER" ; then
+    _failure "No mounted backup medium found. Mount one with 'risks backup mount </dev/device>'"
+fi
+
 if ! _identity_active ; then
     _failure "This command requires an identity to be active"
 fi
 
-# 1 - Open the GPG tomb, and verify target files are here.
-open_tomb "$GPG_TOMB_LABEL" "$IDENTITY"
+# 1 - Open the GPG tomb in the backup, and verify target files are here.
+_run open_tomb_backup "$GPG_TOMB_LABEL"
 
 keygrip="$(gpg -K | grep Keygrip | head -n 1 | cut -d= -f 2 | sed 's/ //g').key"
 _verbose "Keygrip: $keygrip"
 keypath="${HOME}/.tomb/${GPG_TOMB_LABEL}/${keygrip}"
 
 if [[ ! -e "${keypath}" ]]; then
+    close_tomb "${GPG_TOMB_LABEL}"
     _failure "Private key ${keygrip} not found in ${GPG_TOMB_LABEL} tomb"
 fi
 
@@ -28,3 +33,5 @@ risks_hush_rw_command
 
 _message "Importing GPG private key in keyring"
 cp "${keypath}" "${RAMDISK}"/private-keys-v1.d/"${keygrip}" 
+
+_run close_tomb "${GPG_TOMB_LABEL}"
--- src/private_remove_command.sh
+++ src/private_remove_command.sh
@@ -4,10 +4,10 @@ local keygrip keypath
 _set_identity "${args[identity]}"
 
 # Backup/hush and identity checks
-# if ! is_luks_mapper_present "$BACKUP_MAPPER" ; then
-#     _failure "No mounted backup medium found. Mount one with 'risks backup mount </dev/device>'"
-# fi
-#
+if ! is_luks_mapper_present "$BACKUP_MAPPER" ; then
+    _failure "No mounted backup medium found. Mount one with 'risks backup mount </dev/device>'"
+fi
+
 if ! is_hush_mounted ; then
     _failure "The hush device is not mounted. Mount it first and rerun the command."
 fi
@@ -16,8 +16,8 @@ if ! _identity_active ; then
     _failure "This command requires an identity to be active"
 fi
 
-# 1 - Open the GPG tomb
-open_tomb "$GPG_TOMB_LABEL" "$IDENTITY"
+# 1 - Open the GPG tomb backup
+_run open_tomb_backup "$GPG_TOMB_LABEL"
 
 # 3 - Copy/check the private key and revoc certificate, check correcly copied, close GPG tomb
 keygrip="$(gpg -K | grep Keygrip | head -n 1 | cut -d= -f 2 | sed 's/ //g').key"
@@ -31,11 +31,11 @@ if [[ ! -e "${keypath}" ]]; then
     sudo chattr +i "${keypath}" 
 fi
 
-close_tomb "$GPG_TOMB_LABEL"
-
 if [[ ! -e ${keypath} ]]; then
     _failure "Private key file should be found in $GPG_TOMB_LABEL tomb after copy, aborting deletion"
 fi
+
+_run close_tomb "$GPG_TOMB_LABEL"
 
 # 2 - Set the partition read-write and remove from the gpg keyring
 risks_hush_rw_command
