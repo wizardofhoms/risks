42d97e622e6a05389ef3547b797e1508db68c1ee
wizardofhoms <wizardofhoms@proton.me>
2023-02-23 02:07:12 +0100

    More fixes

--- src/private_remove_command.sh
+++ src/private_remove_command.sh
@@ -3,7 +3,11 @@ local keygrip keypath
 
 _set_identity "${args[identity]}"
 
-# Hush and identity checks
+# Backup/hush and identity checks
+if ! is_luks_mapper_present "$BACKUP_MAPPER" ; then
+    _failure "No mounted backup medium found. Mount one with 'risks backup mount </dev/device>'"
+fi
+
 if ! is_hush_mounted ; then
     _failure "The hush device is not mounted. Mount it first and rerun the command."
 fi
@@ -14,6 +18,11 @@ fi
 
 # 2 - Set the partition read-write and remove from the gpg keyring
 risks_hush_rw_command
+
+# 3 - Copy/check the private key and revoc certificate, check correcly copied, close GPG tomb
+keygrip="$(gpg -K | grep Keygrip | head -n 1 | cut -d= -f 2 | sed 's/ //g').key"
+_verbose "Keygrip: $keygrip"
+keyring_path="${RAMDISK}/private-keys-v1.d/${keygrip}" 
 
 _message "Wiping private GPG key ${keygrip} from keyring"
 _run wipe -rf "${keyring_path}" \
